b1fd227f76360fea8af38cb8c34d08ecb9522e13
Stheven <sthevenalvess@gmail.com>
2024-05-29 19:16:35 -0300

    initial config

---
+++ src/main/java/com/sthev/agregador_investimentos/AgregadorInvestimentosApplication.java
@@ -0,0 +1,13 @@
+package com.sthev.agregador_investimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadorInvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregadorInvestimentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sthev/agregador_investimentos/AgregadorInvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sthev.agregador_investimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadorInvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
