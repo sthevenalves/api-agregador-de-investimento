33f9d4797ad53776e69694078267dfbd69059718
Stheven <sthevenalvess@gmail.com>
2024-07-19 12:16:01 -0300

    feat: create BrapiClient to consume data

---
+++ src/main/java/com/sthev/agregador_investimentos/client/BrapiClient.java
@@ -0,0 +1,15 @@
+package com.sthev.agregador_investimentos.client;
+
+import com.sthev.agregador_investimentos.client.dto.BrapiResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name="BrapiClient", url="https://brapi.dev")
+public interface BrapiClient {
+
+    @GetMapping("/api/quote/{stockId}")
+    BrapiResponseDto getQuote(@RequestParam("token") String token,
+                              @PathVariable("stockId") String stockId);
+}
