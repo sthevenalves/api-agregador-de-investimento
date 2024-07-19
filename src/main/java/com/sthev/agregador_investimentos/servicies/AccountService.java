596eb5b945bece1b06c087d0369dc1471a41af83
Stheven <sthevenalvess@gmail.com>
2024-07-19 12:18:23 -0300

    feat: add method to calculate total price

--- src/main/java/com/sthev/agregador_investimentos/servicies/AccountService.java
+++ src/main/java/com/sthev/agregador_investimentos/servicies/AccountService.java
@@ -1,5 +1,6 @@
 package com.sthev.agregador_investimentos.servicies;
 
+import com.sthev.agregador_investimentos.client.BrapiClient;
 import com.sthev.agregador_investimentos.domain.AccountStock;
 import com.sthev.agregador_investimentos.domain.AccountStockId;
 import com.sthev.agregador_investimentos.domain.dto.AccountStockResponseDto;
@@ -8,6 +9,7 @@ import com.sthev.agregador_investimentos.repositories.AccountRepository;
 import com.sthev.agregador_investimentos.repositories.AccountStockRepository;
 import com.sthev.agregador_investimentos.repositories.StockRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -18,12 +20,17 @@ import java.util.UUID;
 @Service
 public class AccountService {
 
+    @Value("#{environment.TOKEN}")
+    private String TOKEN;
+
     @Autowired
     private AccountRepository accountRepository;
     @Autowired
     private StockRepository stockRepository;
     @Autowired
     private AccountStockRepository accountStockRepository;
+    @Autowired
+    private BrapiClient brapiClient;
 
     public void associateStock(String accountId, AssociateAccountStockDto dto){
         var account = accountRepository.findById(UUID.fromString(accountId))
@@ -49,7 +56,15 @@ public class AccountService {
 
         return account.getAccountStocks()
                 .stream()
-                .map(as -> new AccountStockResponseDto(as.getStock().getStockId(), as.getQuantity(), 0.0))
-                .toList();
+                .map(as -> new AccountStockResponseDto(as.getStock().getStockId(),
+                        as.getQuantity(), getTotal(as.getStock().getStockId(),
+                        as.getQuantity())
+                )).toList();
+    }
+
+    private double getTotal(String stockId, Integer quantity) {
+        var response = brapiClient.getQuote(TOKEN, stockId);
+        var price = response.result().get(0).regularMarketPrice();
+        return quantity * price;
     }
 }
